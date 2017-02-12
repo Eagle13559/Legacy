1d26d9f69cf366a65de470ff5a533a27bd71507f
Eagle13559 <justinawunderful@gmail.com>
2017-02-12 13:05:12 -0700

    Added enemy controller and added folder system

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyController : MonoBehaviour {
+
+    /// <summary>
+    /// Represents the total distance an enemy can patrol
+    /// </summary>
+    [SerializeField]
+    private float deltaPatrol;
+
+    /// <summary>
+    /// Represents the total distance an enemy can chase a player in positive x direction
+    /// </summary>
+    [SerializeField]
+    private float deltaPosChase;
+
+    /// <summary>
+    /// Represents the total distance an enemy can chase a player in negative y direction
+    /// </summary>
+    [SerializeField]
+    private float deltaNegChase;
+
+    /// <summary>
+    /// Contains the original starting location and the max patrol location
+    /// </summary>
+    private Tuple<Transform, Transform> patrolPoints;
+
+    /// <summary>
+    /// Contains the min and max patrol location
+    /// </summary>
+    private Tuple<Transform, Transform> chasePoints;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour {
     public float jumpHeight = 2f;
     public float walkSpeed = 3f;
     private CharacterController2D _controller;
+    private AnimationController2D _animator;
 
     // Use this for initialization
     void Start () {
         _controller = gameObject.GetComponent<CharacterController2D>();
+        _animator = gameObject.GetComponent<AnimationController2D>();
         gameCamera.GetComponent<CameraFollow2D>().startCameraFollow(this.gameObject);
     }
 	
@@ -23,14 +25,23 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetAxis("Horizontal") < 0)
         {
             velocity.x = walkSpeed * -1;
+            _animator.setFacing("Right");
+            _animator.setAnimation("p_run");
         }
         else if (Input.GetAxis("Horizontal") > 0)
         {
             velocity.x = walkSpeed;
+            _animator.setFacing("Left");
+            _animator.setAnimation("p_run");
+        }
+        else
+        {
+            _animator.setAnimation("p_idle");
         }
         if ( Input.GetAxis("Jump") > 0 && _controller.isGrounded )
         {
             velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
+            _animator.setAnimation("p_jump");
         }
         velocity.y += gravity * Time.deltaTime;
         _controller.move( velocity * Time.deltaTime );
