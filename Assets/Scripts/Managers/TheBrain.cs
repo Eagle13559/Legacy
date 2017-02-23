996f6f6fcbbd9ce1e460b1285984e526fcaff55c
Nate Merrill <nate.workplace@gmail.com>
2017-02-22 20:16:19 -0700

    Added brain, player animations

---
+++ Assets/Scripts/Managers/TheBrain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBrain : MonoBehaviour
+{
+    void Start ()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    /// <summary>
+    /// Keeps track of the players money from instance to instance.
+    /// </summary>
+    public long PlayersMoney;
+
+    /// <summary>
+    /// Keeps track of overall time from instance to instance. 
+    /// </summary>
+    public float Time;
+
+    /// <summary>
+    /// Keeps track of the index that will be loaded on the next load level call
+    /// </summary>
+    public int nextSceneIndex;
+}
