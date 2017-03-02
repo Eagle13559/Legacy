00288a5b0776884565a10b1c39fadc2a10fea6f1
kalvansears <u0860062@utah.edu>
2017-03-01 20:59:01 -0700

    Help/Option Menu in Tester Update
    Almost done wiht it with new code form Matt

---
+++ Assets/Scripts/UI/ButtonObjectToggler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ButtonObjectToggler : MonoBehaviour {
+
+    public GameObject ObjectToToggle;
+
+	public void Toggle()
+    {
+        ObjectToToggle.SetActive(!ObjectToToggle.activeSelf);
+    }
+}
