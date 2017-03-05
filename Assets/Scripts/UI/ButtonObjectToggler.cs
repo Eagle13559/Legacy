560a3035df5c1cc4b4bf0e2c9fe1c439bb1be56b
kalvansears <u0860062@utah.edu>
2017-03-05 15:21:06 -0700

    Close Doors Animation. Help Menu work and new assets in tester scene.

--- Assets/Scripts/UI/ButtonObjectToggler.cs
+++ Assets/Scripts/UI/ButtonObjectToggler.cs
@@ -3,9 +3,14 @@
 public class ButtonObjectToggler : MonoBehaviour {
 
     public GameObject ObjectToToggle;
+    public GameObject ObjectToToggle2;
+ 
 
-	public void Toggle()
+    public void Toggle()
     {
         ObjectToToggle.SetActive(!ObjectToToggle.activeSelf);
+        ObjectToToggle2.SetActive(!ObjectToToggle2.activeSelf);
+
+
     }
 }
