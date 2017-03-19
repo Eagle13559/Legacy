ab9a96f68e64b9ea7e2461abc30391213a92b842
kalvansears <u0860062@utah.edu>
2017-03-19 09:38:25 -0600

    Three different types of speech boxes in KalvansTesting to choose from. Mainmenu background update. Also cut the Shopkeeper sprites so I could use one for my speech box testing.

---
+++ Assets/Scripts/UI/TextBoxManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    private bool isTyping = false;
+    private bool cancelTyping = false;
+
+    public float typeSpeed;
+
+    public bool isActive;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    void Update()
+    {
+        //theText.text = textLines[currentLine];
+
+        if(!isActive)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if(!isTyping)
+            {
+                currentLine += 1;
+
+                if (currentLine > endAtLine)
+                {
+                    DisableTextBox();
+                
+                }
+                else
+                {
+                    StartCoroutine(TextScroll(textLines[currentLine]));
+                }
+            }
+            else if (isTyping && !cancelTyping)
+            {
+                cancelTyping = true;
+            }
+
+            
+        }
+
+        if(currentLine > endAtLine)
+        {
+            DisableTextBox();
+        }
+    }
+
+
+    private IEnumerator TextScroll (string lineofText)
+    {
+        int letter = 0;
+        theText.text = "";
+        isTyping = true;
+        cancelTyping = false;
+        while (isTyping && !cancelTyping && (letter < lineofText.Length - 1))
+        {
+            theText.text += lineofText[letter];
+            letter += 1;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        theText.text = lineofText;
+        isTyping = false;
+        cancelTyping = false;
+    
+    }
+
+
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+        StartCoroutine(TextScroll(textLines[currentLine]));
+
+    }
+
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+    }
+}
