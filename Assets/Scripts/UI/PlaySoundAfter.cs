74d60ec66a63cf1002b3462f86d0293b2939ae65
kalvansears <u0860062@utah.edu>
2017-04-05 00:47:39 -0600

    SFX slider now working properly. Placed candy coins around level 1. Sparkly updates

---
+++ Assets/Scripts/UI/PlaySoundAfter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlaySoundAfter : MonoBehaviour {
+
+    public AudioClip ClickSource;
+    AudioSource audio;
+
+    private Slider _slider;
+
+    void Start()
+    {
+        _slider = this.GetComponent<Slider>();
+        audio = this.GetComponent<AudioSource>();
+    } 
+
+    public void PlayVolumeCheck()
+    {
+        Vector3 temp = new Vector3(9f, 5f);
+        Debug.Log("ping");
+        audio.PlayOneShot(ClickSource, _slider.value);
+    }
+
+}
