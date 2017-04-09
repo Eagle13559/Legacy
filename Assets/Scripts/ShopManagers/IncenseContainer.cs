afcfbc17ae71b6de65e13a80c7220347a0b32523
sqwigs <nate.workplace@gmail.com>
2017-04-09 15:48:30 -0600

    Incense purchasing in place, when at zero a bug allows you to still purchase. Still need to put in icon for how many incense candles you are purchasing.

---
+++ Assets/Scripts/ShopManagers/IncenseContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct IncenseContainer {
+
+    public TheBrain.IncenseTypes itemType { get; private set; }
+    public int numOfItems { get; private set; }
+
+    public IncenseContainer(TheBrain.IncenseTypes type, int num)
+    {
+        itemType = type;
+        numOfItems = num;
+    }
+}
