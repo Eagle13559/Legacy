e732a1caf2b1399da19034e761bf9d4bb3e739e7
Eagle13559 <justinawunderful@gmail.com>
2017-04-12 18:29:16 -0600

    Worked on conveyor belt

---
+++ Assets/Scripts/Background/ConveyorBelt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour {
+    [SerializeField]
+    float directionX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Vector3 direction = new Vector3(1f, 0f, 0f);
+        direction *= directionX;
+        collision.transform.position += direction;
+    }
+}
