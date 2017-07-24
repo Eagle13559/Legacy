d3b720d052f364e1a74680521cd91d97119f64ba
kalvansears <kalvansears@gmail.com>
2017-07-24 10:19:31 -0600

    Player settings in the ui scripts saves the volume to PlayerPrefs.

---
+++ Assets/Scripts/UI/PlayerSettings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+public class PlayerSettings : MonoBehaviour
+{
+
+    public Slider masterVol;
+    public Slider musicVol;
+    public Slider sfxVol;
+
+    public AudioMixer masterMixer;
+
+    void Start()
+    {
+        masterVol.value = PlayerPrefs.GetFloat("masterVol", 1); // load vol and make default 1
+        musicVol.value = PlayerPrefs.GetFloat("musicVol", 1);
+        sfxVol.value = PlayerPrefs.GetFloat("sfxVol", 1);
+
+    }
+
+    private void Update()
+    {
+        masterMixer.SetFloat("masterVol", masterVol.value);
+    }
+
+
+
+    public void SaveSets()
+    {
+
+        PlayerPrefs.SetFloat("masterVol", masterVol.value);
+       // PlayerPrefs.SetFloat("musicVol", musicVol.value);
+       // PlayerPrefs.SetFloat("sfxVol", sfxVol.value);
+        Debug.Log("Master Volume set");
+    }
+
+}
+
