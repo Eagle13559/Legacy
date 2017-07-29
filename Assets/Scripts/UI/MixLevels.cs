b28c14cfaa4c3c8dd48089d85452f28d36e5522e
kalvansears <kalvansears@gmail.com>
2017-07-29 10:53:03 -0600

    Testing level changes. Working on muting.

--- Assets/Scripts/UI/MixLevels.cs
+++ Assets/Scripts/UI/MixLevels.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MixLevels : MonoBehaviour {
 
     public AudioMixer masterMixer;
+    public Slider masterSlider;
+    public Toggle masterTog;
 
     public void SetMasterLvL (float masterLvl)
     {
-        masterMixer.SetFloat("masterVol", masterLvl);
-  
+        masterMixer.SetFloat("masterVol", masterLvl);  
     }
 
     public void SetSfxLvl (float sfxLvl)
@@ -23,4 +25,21 @@ public class MixLevels : MonoBehaviour {
     {
         masterMixer.SetFloat("musicVol", musicLvl);
     }
+
+    public void MuteMaster ()
+    {
+        if (masterTog.isOn == true)
+        {
+            masterMixer.SetFloat("masterVol", -60f);
+            Debug.Log("mute");
+        }
+
+        else
+        {
+            Debug.Log(masterSlider.value);
+            masterMixer.SetFloat("masterVol", masterSlider.value);
+            Debug.Log("unmute");
+        }
+        
+       }
 }
